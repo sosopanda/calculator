798bc23ff600947ccf774b540bf33bb9bb34f949
sosopanda <mcamarasantacreu@gmail.com>
2025-08-05 11:33:05 +0200

    added keyboard support

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const mostrar = document.createElement("p");
 const results = document.querySelector("#results");
 
-const checkOperators = '/+-X';
+const checkOperators = "/+-X";
+const checkNumbers = "0123456789"
 
 let num1 = '';
 let num2 = '';
@@ -20,90 +21,29 @@ const cleanButton = document.querySelector("#clean");
 const pointButton = document.querySelector("#point");
 const backButton = document.querySelector("#back");
 
-//adding the first and second number
+//calling the funciton to add a number when a number is clicked
 numberButtons.forEach((button) => {
-
-    //checking if screen needs to be cleaned
     button.addEventListener("click",() => {
-        if(reset == true)
-            mostrar.textContent = ''; reset = false;
-
-        mostrar.textContent = mostrar.textContent + button.id;
-
-        //first number
-        if(!operating){
-            if(!firsNumber){
-                num1 = num1 + button.id;
-            } else {
-                mostrar.textContent = button.id;
-                num1 = button.id; 
-                firsNumber = false;
-            }     
-            
-        //second number    
-        } else
-            if(!firsNumber){
-                num2 = num2 + button.id;
-            } else {
-                num2 = button.id; 
-                firsNumber = false;
-            } 
-
-        validInput = true;
+        addNumber(button.id);
     });
 });
 
-//adding the operator and resolving if needed
+//calling the function to add an operator and resolve if needed when an operator is clicked
 operatorButtons.forEach((button) => {
     button.addEventListener("click",() => {
-        if(validInput){
-            pointed = false;
-
-            //save first number and adding the first operator
-            if(!operating && button.id != "="){
-                firsNumber = true;
-                operating = true;
-                operator = button.id;
-                mostrar.textContent = mostrar.textContent + button.id;
-                validInput = false;
-
-            //resolving when = pressed    
-            } else if (operating && button.id == "=" && num2 != ''){
-                mostrar.textContent = resolve(num1,num2,operator)
-                num1 = mostrar.textContent;
-                num2 = 0;
-                operating = false;
-                firsNumber = true;
-            
-            //resolve to keep going with more operators   
-            } else if(operating){
-                mostrar.textContent = resolve(num1,num2,operator)  + button.id;
-                operator = button.id;
-                num1 = mostrar.textContent;
-                num2 = '0';
-                validInput = false;
-                firsNumber = true;
-
-            //handling unwanted scenarios
-            } else{
-                mostrar.textContent = "ERROR!";
-                reset = true;
-                num1 = '0';
-                num2 = '0';
-                operating = false;
-                firsNumber = true;
-            }
-
-        //handling unwanted scenarios
-        } else {
-            mostrar.textContent = "ERROR!";
-            reset = true;
-            num1 = '0';
-            num2 = '0';
-            operating = false;
-        }
+        addOperator(button.id);
     });
 });
+
+//keyboard controls
+document.addEventListener("keydown", function(event) {
+    if(checkNumbers.includes(event.key))
+        addNumber(event.key);
+    if(checkOperators.includes(event.key))
+        addOperator(event.key);
+    if(event.key == "Enter")
+        addOperator('=');
+}, true);
 
 //adding a decimal and controlling not adding two points to the same number
 pointButton.addEventListener("click",() => {
@@ -133,7 +73,7 @@ backButton.addEventListener("click",() => {
         if(num2 != ''){
             num2 = num2.slice(0,-1);
             mostrar.textContent = mostrar.textContent.slice(0,-1);
-        } else if(mostrar.textContent.includes(checkOperators)){
+        } else if(checkOperators.includes(mostrar.textContent)){
             mostrar.textContent = mostrar.textContent.slice(0,-1);
             operating = false;
 
@@ -144,6 +84,87 @@ backButton.addEventListener("click",() => {
         }
     }
 });
+
+//adding the operator and resolving if needed
+function addOperator(op) {
+    if(validInput){
+        pointed = false;
+
+        //save first number and adding the first operator
+        if(!operating && op != "="){
+            firsNumber = true;
+            operating = true;
+            operator = op;
+            mostrar.textContent = mostrar.textContent + op;
+            validInput = false;
+
+        //resolving when = pressed    
+        } else if (operating && op == "=" && num2 != ''){
+            mostrar.textContent = resolve(num1,num2,operator)
+            num1 = mostrar.textContent;
+            num2 = 0;
+            operating = false;
+            firsNumber = true;
+            
+        //resolve to keep going with more operators   
+        } else if(operating){
+            mostrar.textContent = resolve(num1,num2,operator)  + op;
+            operator = op;
+            num1 = mostrar.textContent;
+            num2 = '0';
+            validInput = false;
+            firsNumber = true;
+
+        //handling unwanted scenarios
+        } else{
+            mostrar.textContent = "ERROR!";
+            reset = true;
+            num1 = '0';
+            num2 = '0';
+            operating = false;
+            firsNumber = true;
+        }
+
+    //handling unwanted scenarios
+    } else {
+        mostrar.textContent = "ERROR!";
+        reset = true;
+        num1 = '0';
+        num2 = '0';
+        operating = false;
+    }
+}
+
+//adding the first or the second number
+function addNumber(num) {
+
+    //checking if screen needs to be cleaned
+    if(reset == true)
+            mostrar.textContent = ''; reset = false;
+
+        mostrar.textContent = mostrar.textContent + num;
+
+        //first number
+        if(!operating){
+            if(!firsNumber){
+                num1 = num1 + num;
+            } else {
+                mostrar.textContent = num;
+                num1 = num; 
+                firsNumber = false;
+            }     
+            
+        //second number    
+        } else
+            if(!firsNumber){
+                num2 = num2 + num;
+            } else {
+                num2 = num; 
+                firsNumber = false;
+            } 
+
+        validInput = true;
+}
 
 //function to resolve operations depending on the operator
 function resolve(a,b,op){
